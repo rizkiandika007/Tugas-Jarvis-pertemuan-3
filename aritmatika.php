251e4669c7ddaccd3465830a4a2c0644affc9bc0
rizkiandika077 <141980012+rizkiandika077@users.noreply.github.com>
2024-05-17 21:53:40 +0700

    jarvis

--- aritmatika.php
+++ aritmatika.php
@@ -1,26 +1,18 @@
+<?php 
+$nilai1 = 10;
+$nilai2 = 5;
 
-<?php
-function hitung_aritmatika($operasi, $angka1, $angka2) {
-    if ($operasi == 'bagi' && $angka2 == 0) {
-        echo "Tidak dapat membagi dengan nol";
-        return;
-    }
+echo $nilai1 * $nilai2;
+//perkalian
+echo "<br>";
 
-    if ($operasi == 'tambah') {
-        echo "Hasil penjumlahan dari $angka1 dan $angka2 adalah: " . ($angka1 + $angka2) . "<br>";
-    } elseif ($operasi == 'kurang') {
-        echo "Hasil pengurangan dari $angka1 dan $angka2 adalah: " . ($angka1 - $angka2) . "<br>";
-    } elseif ($operasi == 'bagi') {
-        echo "Hasil pembagian dari $angka1 dengan $angka2 adalah: " . ($angka1 / $angka2) . "<br>";
-    } elseif ($operasi == 'kali') {
-        echo "Hasil perkalian dari $angka1 dengan $angka2 adalah: " . ($angka1 * $angka2) . "<br>";
-    } else {
-        echo "Operasi tidak valid";
-    }
-}
+echo $nilai1 + $nilai2 ;
+//tambahan
+echo $nilai1 / $nilai2 ;
+//pembagian
+echo $nilai1 - $nilai2 ;
+//pengurangan
+echo $nilai1 % $nilai2;
+// modulus (nilai sisa)
 
-hitung_aritmatika('tambah', 4, 3);
-hitung_aritmatika('kurang', 6, 3);
-hitung_aritmatika('bagi', 12, 2);
-hitung_aritmatika('kali', 5, 3);
 ?>
